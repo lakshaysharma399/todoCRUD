a7e2856f832c53af0716dc62ea60d1d0b704a9f3
Lakshaysoulpay <lakshay@soulpay.co.in>
2025-10-18 16:50:48 +0530

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,48 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, settodo] = useState("")
+  const [todos, settodos] = useState([])
+
+
+  function handleChange(e) {
+    settodo(e.target.value)
+  }
+
+
+  function addtodo() {
+    settodos([...todos, todo]);
+
+    settodo("")
+  }
+
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <label htmlFor="">TODO</label>
+        <input type="text" placeholder='todo' onChange={handleChange} value={todo} />
+        <button onClick={addtodo}>Add todo</button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <ul>
+
+          {todos.map((name, index) => (
+
+            <li key={index}> {name}</li>
+
+          ))}
+
+        </ul>
+
+
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
